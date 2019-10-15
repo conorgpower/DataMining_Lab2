d1835085e661a4e2648c33e47724f98eed74b9f8
Conor <conorgpower@gmail.com>
2019-10-15 17:33:13 +0100

    Normality testing completed

---
+++ dataExplorationHorsepower.py
@@ -0,0 +1,34 @@
+# Q. Import data from "BSCY4.csv".
+# A.
+import pandas as pd 
+df = pd.read_csv('BSCY4_Lab_2.csv')
+print(df.head())
+
+# Q. Assess normality of horsepower values.
+# Kurtosis
+skewness = df['horsepower'].skew(axis = 0, skipna = True)
+print("Skewness: ", skewness)
+# Skewness
+kurtosis = df['horsepower'].kurtosis()
+print("Kurtosis: ", kurtosis)
+
+# (1) Visual Inspection
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.show(sns.distplot(df['horsepower']))
+
+# (2) NHST
+from scipy import stats
+shapiroHorsepower = stats.shapiro(df['horsepower'])
+print("Shapiro-Wilk Test for 'horsepower': ",shapiroHorsepower)
+
+# (3) Q-Q Plot
+plt.show(stats.probplot(df['horsepower'], dist="norm", plot=plt))
+
+# Q. Do the numbers appear to come from a normal distribution? 
+# If not, can a transformation be applied so that its result is normal?
+# A. The skewness indicates that the distribution is not normal.
+# The kurtosis indictes that the data is not normal.
+# Upon visual inspection we can see the data appears as though it is not normal.
+# The null hypothesis significance testing proves the data is not normal.
+# The Q-Q plot confirms the above.
